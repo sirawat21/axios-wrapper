151c87822caac0ae7f71b9d3ffacad16d1000577
sirawat21 <sirawat21@gmail.com>
2023-11-17 14:12:39 +1100

    add sample file in test

---
+++ src/test/AxiosWrapper.test.ts
@@ -0,0 +1,51 @@
+import AxiosWrapper from "../AxiosWrapper";
+
+const client = new AxiosWrapper({ baseURL: "https://reqres.in" });
+
+describe("AxiosWrapper class", () => {
+    test("Http GET method", async () => {
+        const result = await client.get("/api/users");
+        expect(result).toBeDefined;
+    });
+
+    test("Http POST method", async () => {
+        const result = await client.post("/api/users", {
+            name: "morpheus",
+            job: "leader",
+        });
+        expect(result).toBeDefined;
+    });
+
+    test("Http PUT method", async () => {
+        const result = await client.put("/api/users/2", {
+            name: "morpheus",
+            job: "zion resident",
+        });
+        expect(result).toBeDefined;
+    });
+
+    test("Http PATCH method", async () => {
+        const result = await client.patch("/api/users/2", {
+            name: "morpheus",
+            job: "resident zion",
+        });
+        expect(result).toBeDefined;
+    });
+
+    test("Request Interceptor", async () => {
+        const result = client.setRequestInterceptor((request) => request);
+        await client.get("/api/users");
+        expect(result).toThrowErrorMatchingInlineSnapshot;
+    });
+
+    test("Response Interceptor", async () => {
+        const result = client.setResponseInterceptor((response) => response);
+        await client.get("/api/users");
+        expect(result).toThrowErrorMatchingInlineSnapshot;
+    });
+
+    test("Terminate instant", async () => {
+        const result = client.terminateConnection();
+        expect(result).toBeNull;
+    });
+});
